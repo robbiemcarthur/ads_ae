da234dc7b0baf09fc402d7128242aa2d4aaae150
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-03-11 11:03:03 +0000

    Initial commit

---
+++ CountedElement.java
@@ -0,0 +1,25 @@
+public class CountedElement<E extends Comparable<E>> implements Comparable<CountedElement<E>> {
+	private E element;
+	private int count;
+
+	public CountedElement(E e, int count){
+		//constructor - to complete
+	}
+	
+	public CountedElement(E e){
+		//constructor - to complete
+	}
+
+	//add getters and setters
+	
+	//add toString() method
+	
+	
+
+	
+	public int compareTo(CountedElement<E> sC1) {
+    
+		//to complete
+	}
+
+}
---
+++ WordProcessor.java
@@ -0,0 +1,36 @@
+
+//import classes for file input - scanner etc.
+import java.io.FileNotFoundException;
+import java.util.Set;
+//import implementing set (eg. TreeSet)
+
+
+
+
+public class WordProcessor {
+	private static <E> String displaySet(Set<E> inputSet){
+		//implement this static method to create a
+		// String representation of set - 5 comma separated elements per line
+		// assume that type E has a toString method
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//create a set of type String called wordSet
+		//create a set of type CountedElement<String> called countedWordSet 
+		
+		//for each input file (assume 3 arguments, each the name of a file)
+		//  for each word w
+		//     if wordset doesnt contain w:
+		//        add w to wordset
+	    //        add new element to countedWordSet
+		//     else
+		//        increment numeric part of element in countedWordSet containing w
+		
+
+	System.out.println(displaySet(countedWordSet));
+
+	}
+}
