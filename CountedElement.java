3ff7520260d67eb49a7522b133dc2378a26c50ae
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-03-14 17:02:57 +0000

    works but need to sort into ascending order then print five lines

--- CountedElement.java
+++ CountedElement.java
@@ -3,23 +3,54 @@ public class CountedElement<E extends Comparable<E>> implements Comparable<Count
 	private int count;
 
 	public CountedElement(E e, int count){
-		//constructor - to complete
+		this.element = e;
+		this.count = count;
 	}
 	
 	public CountedElement(E e){
-		//constructor - to complete
+		this.element = e;
 	}
 
 	//add getters and setters
+	public E getElement() 
+	{
+		return element;
+	}
+	
+	public void setElement(E e) 
+	{
+		this.element = e;
+	}
+	
+	public int getCount() 
+	{
+		return count;
+	}
+	
+	public void setCount(int n) 
+	{
+		this.count = n;
+	}
+	
+	public void incrementCount() {
+		count++;
+	}
 	
 	//add toString() method
-	
-	
-
+	public String toString() {
+		String output = ("("+getElement()+","+getCount()+") ");
+		return output;
+	}
 	
 	public int compareTo(CountedElement<E> sC1) {
-    
-		//to complete
+	    if(sC1==this)
+	    {
+	    	return 1;
+	    }
+	    else
+	    {
+	    	return 0;
+	    }
 	}
 
 }
--- WordProcessor.java
+++ WordProcessor.java
@@ -1,6 +1,9 @@
 
-//import classes for file input - scanner etc.
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 //import implementing set (eg. TreeSet)
 
@@ -8,29 +11,102 @@ import java.util.Set;
 
 
 public class WordProcessor {
+
 	private static <E> String displaySet(Set<E> inputSet){
 		//implement this static method to create a
 		// String representation of set - 5 comma separated elements per line
 		// assume that type E has a toString method
+
+		String word = "";
+		int n = 0;
+
+		for(E element:inputSet) {
+			if(n==5)
+			{
+				word+="\n\n";
+				n=0;
+			}
+			word+=element.toString()+", \n";
+		}
+		word+="\n";
+		return word;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String [] files = new String[] {"file0.txt", "file1.txt", "file2.txt"};
+		Set<String> wordSet = new HashSet<String>();
+		Set <CountedElement<String>> countedWordSet = new HashSet<CountedElement<String>>();
+		int instance = 1;
+		for(String fin:files)
+		{
+			File fileIn = new File(fin);
+			try {
+				Scanner s = new Scanner(fileIn);
+				while(s.hasNextLine())
+				{
+					String[] words = s.nextLine().split(" ");
+					for(String w:words)
+					{
+						if(!wordSet.contains(w)||wordSet.isEmpty())
+						{
+//							System.out.println("No instance of " + w + "!");        // TESTING
+							wordSet.add(w);
+							countedWordSet.add(new CountedElement<String>(w, instance));
+						}
+						else
+						{
+							for(CountedElement<String>ce:countedWordSet)
+							{
+								if(ce.getElement().equals(w))
+								{
+									ce.incrementCount();
+								}
+							}
+						}
+					}
+				}
+				s.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 		//create a set of type String called wordSet
 		//create a set of type CountedElement<String> called countedWordSet 
-		
+
 		//for each input file (assume 3 arguments, each the name of a file)
 		//  for each word w
 		//     if wordset doesnt contain w:
 		//        add w to wordset
-	    //        add new element to countedWordSet
+		//        add new element to countedWordSet
 		//     else
+
 		//        increment numeric part of element in countedWordSet containing w
 		
-
-	System.out.println(displaySet(countedWordSet));
-
+		// TESTING STRING OUTPUT
+//		int c = 0;
+//		for(CountedElement<String> wrd:countedWordSet)
+//		{
+//			if(c!=5)
+//			{
+//			System.out.print("("+wrd.getElement() + "," + wrd.getCount()+"), ");
+//			c++;
+//			}
+//			else
+//			{
+//				System.out.print("\n");
+//				c=0;
+//			}
+//		}
+		
+//		for(CountedElement<String>wrd:countedWordSet)
+//		{
+//			System.out.println(wrd.toString());
+//		}
+		System.out.println(displaySet(countedWordSet));
 	}
 }
